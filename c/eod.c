70c7126ff1d31c71e982a4bbf5d9c1551eb56dc6
zi00iz <zii0qkr@gmail.com>
2024-03-06 11:38:54 +0900

    eod

---
+++ c/eod.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int eod(int x) {
+	if (x % 2 == 0) 
+		printf("%d is even\n", x);
+	else
+		printf("%d is odd\n", x);	
+}
+
+void main() {
+	int x;	
+	printf("Input Number : ");
+	scanf("%d",&x); 
+	eod(x);
+}
+
