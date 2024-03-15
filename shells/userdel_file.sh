5a4346415fd766e6fad0fcb62123d651be48771c
zi00iz <zii0qkr@gmail.com>
2024-03-15 15:50:20 +0900

    userdel_file.sh

---
+++ shells/userdel_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+input="user.dat"
+
+while IFS=',' read -r username uid gid comment
+do
+	userdel "$username"
+	rm -rf /home/$username
+	rm -rf /var/mail/$username
+	echo "Delete $username"
+done < $input
+
+echo
+tail -3 /etc/passwd
